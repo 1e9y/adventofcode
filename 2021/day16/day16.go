39f5f8b252f7448a0cfa33185576d6eebaf1c469
V K <84337571+1e9y@users.noreply.github.com>
2021-12-17 17:51:54 +0300

    solution(2021.16): packet decoder

--- 2021/day16/day16.go
+++ 2021/day16/day16.go
@@ -3,275 +3,200 @@ package day16
 import (
 	"fmt"
 	"math"
-	"strconv"
 	"strings"
 
 	"github.com/1e9y/adventofcode/challenge"
+	"github.com/1e9y/adventofcode/util"
 )
 
-var versionSum int
+type Type int
 
-// 0 = 0000
-// 1 = 0001
-// 2 = 0010
-// 3 = 0011
-// 4 = 0100
-// 5 = 0101
-// 6 = 0110
-// 7 = 0111
-// 8 = 1000
-// 9 = 1001
-// A = 1010
-// B = 1011
-// C = 1100
-// D = 1101
-// E = 1110
-// F = 1111
+const (
+	TypeSum Type = iota
+	TypeProduct
+	TypeMinimum
+	TypeMaximum
+	TypeLiteral
+	TypeGreaterThan
+	TypeLessThan
+	TypeEqualTo
+)
 
-// 100 010 1 00000000001 001010100000000001101010000000000000101111010001111000
-// 4   2   1           1
-// v 4
+type Packet struct {
+	version    int
+	typeID     Type
+	value      int
+	lengthType int
+	length     int
+	subpackets []*Packet
+}
 
-// 001 010 1 00000000001 101010000000000000101111010001111000
-//   1   2 1           1
-// v 5
-
-// 101 010 0 000000000001011 11010001111 000
-//   5   2                11
-// v 10
-
-// 110 100 01111 000
-//   6   4
-// v16
-
-// 011 000 1 00000000010 00000000000000000101100001000101010110001011001000100000000010000100011000111000110100
-//  v3   0             2
-
-// 1:
-// 000 000 0 000000000010110 0001000101010110001011 001000100000000010000100011000111000110100
-//  v0   0                22
-
-// 000 100 01010 10110001011
-//  v0   4
-
-// 101 100 01011
-//  v5   4
-
-// 2:
-// 001 000 1 00000000010 000100011000111000110100
-//  v1   0             2
-
-// 000 100 01100 0111000110100
-//  v0
-
-// 011 100 0110100
-//  v3
-
-func decode(input string) int {
-	// fmt.Println("+ input")
-	// fmt.Println(input)
+func newPacketFromInput(input string) *Packet {
 	builder := strings.Builder{}
 	for _, c := range input {
-		res, err := strconv.ParseInt(string(c), 16, 8)
-		if err != nil {
-			panic(err)
-		}
+		res := util.MustParseInt(string(c), 16)
 		builder.WriteString(fmt.Sprintf("%04b", res))
 	}
 	bits := builder.String()
-	// fmt.Println("+ bits")
-	// fmt.Println(bits)
 
-	packet, _ := readPackage(bits)
-	println(packet)
-	// pack, rams := readPackage(bits)
-	// fmt.Println("+ package")
-	// fmt.Printf("%#v\n", pack)
-	// fmt.Println("+ remains")
-	// fmt.Printf("%#v\n", rams)
-	return calculate(packet)
+	packet, _ := readPacket(bits)
+	return packet
 }
 
-func calculate(packet *Package) int {
-	switch packet.typeID {
+func readPacket(bits string) (*Packet, string) {
+	packet := Packet{
+		lengthType: -1,
+		subpackets: make([]*Packet, 0),
+	}
+
+	version := util.MustParseInt(bits[:3], 2)
+	packet.version = version
+	bits = bits[3:]
+
+	id := util.MustParseInt(bits[:3], 2)
+	packet.typeID = Type(id)
+	bits = bits[3:]
+
+	if id == int(TypeLiteral) {
+		tail := packet.readLiteral(bits)
+		return &packet, tail
+	}
+
+	lenghtType := util.MustParseInt(bits[:1], 2)
+	packet.lengthType = lenghtType
+	bits = bits[1:]
+
+	switch lenghtType {
 	case 0:
+		tail := packet.readPacketByLength(bits)
+		return &packet, tail
+	case 1:
+		tail := packet.readPacketByNumber(bits)
+		return &packet, tail
+	}
+
+	panic(fmt.Errorf("bad packet: %s", bits))
+}
+
+func (packet *Packet) readLiteral(bits string) string {
+	var group, value string
+	for {
+		group = bits[:5]
+		bits = bits[5:]
+		value += group[1:]
+		if group[0] == '0' {
+			break
+		}
+	}
+	packet.value = util.MustParseInt(value, 2)
+	return bits
+}
+
+func (packet *Packet) readPacketByLength(bits string) (tail string) {
+	length := util.MustParseInt(bits[:15], 2)
+	packet.length = length
+	bits = bits[15:]
+
+	var subpacket *Packet
+
+	tail = bits
+	rest := len(bits)
+	consumed := 0
+	for consumed < length {
+		subpacket, tail = readPacket(bits)
+		packet.subpackets = append(packet.subpackets, subpacket)
+		consumed += (rest - len(tail))
+		rest = len(tail)
+		bits = tail
+	}
+
+	return tail
+}
+
+func (packet *Packet) readPacketByNumber(bits string) (tail string) {
+	length := util.MustParseInt(bits[:11], 2)
+	packet.length = length
+	bits = bits[11:]
+
+	var subpacket *Packet
+
+	count := length
+	for count > 0 {
+		subpacket, tail = readPacket(bits)
+		packet.subpackets = append(packet.subpackets, subpacket)
+		bits = tail
+		count--
+	}
+
+	return tail
+}
+
+func (packet *Packet) calculate() int {
+	switch packet.typeID {
+	case TypeSum:
 		sum := 0
 		for _, p := range packet.subpackets {
-			sum += calculate(p)
+			sum += p.calculate()
 		}
 		return sum
-	case 1:
+	case TypeProduct:
 		product := 1
 		for _, p := range packet.subpackets {
-			product *= calculate(p)
+			product *= p.calculate()
 		}
 		return product
-	case 2:
+	case TypeMinimum:
 		value := math.MaxInt64
 		for _, p := range packet.subpackets {
-			nextvalue := calculate(p)
+			nextvalue := p.calculate()
 			if nextvalue < value {
 				value = nextvalue
 			}
 		}
 		return value
-	case 3:
+	case TypeMaximum:
 		value := 0
 		for _, p := range packet.subpackets {
-			nextvalue := calculate(p)
+			nextvalue := p.calculate()
 			if nextvalue > value {
 				value = nextvalue
 			}
 		}
 		return value
-	case 4:
+	case TypeLiteral:
 		return int(packet.value)
-	case 5:
-		if calculate(packet.subpackets[0]) > calculate(packet.subpackets[1]) {
+	case TypeGreaterThan:
+		if packet.subpackets[0].calculate() > packet.subpackets[1].calculate() {
 			return 1
 		}
 		return 0
-	case 6:
-		if calculate(packet.subpackets[0]) < calculate(packet.subpackets[1]) {
+	case TypeLessThan:
+		if packet.subpackets[0].calculate() < packet.subpackets[1].calculate() {
 			return 1
 		}
 		return 0
-	case 7:
-		if calculate(packet.subpackets[0]) == calculate(packet.subpackets[1]) {
+	case TypeEqualTo:
+		if packet.subpackets[0].calculate() == packet.subpackets[1].calculate() {
 			return 1
 		}
 		return 0
 	}
-	panic("bad stuff")
+
+	panic(fmt.Errorf("bad packet type: %v", packet.typeID))
 }
 
-type Package struct {
-	version     int64
-	typeID      int64
-	lengthType  int64
-	lengthValue int64
-	bits        string
-	subpackets  []*Package
-	value       int64
-}
-
-func readPackage(bits string) (*Package, string) {
-	// println("PARSING", bits)
-	pack := Package{
-		subpackets: make([]*Package, 0),
+func (packet *Packet) versions() (sum int) {
+	sum += int(packet.version)
+	for _, p := range packet.subpackets {
+		sum += p.versions()
 	}
-	version, err := strconv.ParseInt(bits[:3], 2, 8)
-	if err != nil {
-		panic(err)
-	}
-	pack.version = version
-	bits = bits[3:]
-	versionSum += int(version)
-
-	id, err := strconv.ParseInt(bits[:3], 2, 8)
-	if err != nil {
-		panic(err)
-	}
-	pack.typeID = id
-	bits = bits[3:]
-
-	// println("TYPE", id, "VERSION", version)
-
-	if id == 4 {
-		pack.lengthType = -1
-		pack.bits = bits
-
-		subbits := ""
-		for {
-			group := bits[:5]
-			bits = bits[5:]
-			subbits += group[1:]
-			if group[0] == '0' {
-				break
-			}
-		}
-		value, err := strconv.ParseInt(subbits, 2, 64)
-		if err != nil {
-			panic(err)
-		}
-		pack.value = value
-		return &pack, bits
-	}
-
-	lenghtType, err := strconv.ParseInt(bits[:1], 2, 8)
-	if err != nil {
-		panic(err)
-	}
-	pack.lengthType = lenghtType
-	bits = bits[1:]
-
-	if lenghtType == 0 {
-		lenghtValue, err := strconv.ParseInt(bits[:15], 2, 64)
-		if err != nil {
-			panic(err)
-		}
-		// total length
-		pack.lengthValue = lenghtValue
-		bits = bits[15:]
-		pack.bits = bits
-
-		origLen := len(bits)
-		consumed := 0
-		var lastBits string = bits
-
-		var subpacket *Package
-		// for consumed <= int(lenghtValue) {
-		for consumed < int(lenghtValue) {
-			subpacket, lastBits = readPackage(bits)
-			// fmt.Println(">", origLen, lenghtValue, lastBits, consumed)
-			consumed += (origLen - len(lastBits))
-			origLen = len(lastBits)
-			pack.subpackets = append(pack.subpackets, subpacket)
-			// fmt.Printf("> %#v\n", subpacket)
-			bits = lastBits
-		}
-		// println("after readling remained:", lastBits)
-		// fmt.Printf("> %#v\n", subpacket)
-		return &pack, lastBits
-	}
-	if lenghtType == 1 {
-		lenghtValue, err := strconv.ParseInt(bits[:11], 2, 64)
-		if err != nil {
-			panic(err)
-		}
-
-		// count
-		pack.lengthValue = lenghtValue
-		pack.bits = bits
-		bits = bits[11:]
-
-		var lastBits string
-		var subpacket *Package
-
-		i := lenghtValue
-		for i > 0 {
-			// println()
-			// println(i, bits)
-			subpacket, lastBits = readPackage(bits)
-			pack.subpackets = append(pack.subpackets, subpacket)
-			bits = lastBits
-			// fmt.Printf("> %v %#v\n", i, subpacket)
-			i--
-		}
-
-		return &pack, lastBits
-	}
-
-	panic("bad input")
+	return
 }
 
 func A(input *challenge.Challenge) int {
-	versionSum = 0
-	decode(<-input.Lines())
-	return versionSum
+	return newPacketFromInput(<-input.Lines()).versions()
 }
 
 func B(input *challenge.Challenge) int {
-	versionSum = 0
-	return decode(<-input.Lines())
+	return newPacketFromInput(<-input.Lines()).calculate()
 }
--- 2021/day16/day16_test.go
+++ 2021/day16/day16_test.go
@@ -9,59 +9,37 @@ import (
 
 var testCasesA = []struct {
 	input  string
-	expect []int
+	expect int
 }{
-	{
-		"D2FE28",
-		[]int{6, 0},
-	},
-	{
-		"38006F45291200",
-		[]int{9, 0},
-	},
-	{
-		"EE00D40C823060",
-		[]int{14, 0},
-	},
-	{
-		"8A004A801A8002F478",
-		[]int{16, 315},
-	},
-	{
-		"620080001611562C8802118E34",
-		[]int{12, 315},
-	},
-	{
-		"C0015000016115A2E0802F182340",
-		[]int{23, 315},
-	},
-	{
-		"A0016C880162017C3686B18A3D4780",
-		[]int{31, 315},
-	},
-
-	{"C200B40A82", []int{14, 3}},
+	{"D2FE28", 6},
+	{"38006F45291200", 9},
+	{"EE00D40C823060", 14},
+	{"8A004A801A8002F478", 16},
+	{"620080001611562C8802118E34", 12},
+	{"C0015000016115A2E0802F182340", 23},
+	{"A0016C880162017C3686B18A3D4780", 31},
+	{"C200B40A82", 14},
 }
 
 var testCasesB = []struct {
 	input  string
-	expect []int
+	expect int
 }{
-	{"C200B40A82", []int{0, 3}},
-	{"04005AC33890", []int{0, 54}},
-	{"880086C3E88112", []int{0, 7}},
-	{"CE00C43D881120", []int{0, 9}},
-	{"D8005AC2A8F0", []int{0, 1}},
-	{"F600BC2D8F", []int{0, 0}},
-	{"9C005AC2F8F0", []int{0, 0}},
-	{"9C0141080250320F1802104A08", []int{0, 1}},
+	{"C200B40A82", 3},
+	{"04005AC33890", 54},
+	{"880086C3E88112", 7},
+	{"CE00C43D881120", 9},
+	{"D8005AC2A8F0", 1},
+	{"F600BC2D8F", 0},
+	{"9C005AC2F8F0", 0},
+	{"9C0141080250320F1802104A08", 1},
 }
 
 func TestSum(t *testing.T) {
 	for _, c := range testCasesA {
 		t.Run(c.input, func(t *testing.T) {
 			input := challenge.ReadChallengeFromLiteral(c.input)
-			assert.Equal(t, c.expect[0], A(input))
+			assert.Equal(t, c.expect, A(input))
 		})
 	}
 }
@@ -70,13 +48,12 @@ func TestResult(t *testing.T) {
 	for _, c := range testCasesB {
 		t.Run(c.input, func(t *testing.T) {
 			input := challenge.ReadChallengeFromLiteral(c.input)
-			assert.Equal(t, c.expect[1], B(input))
+			assert.Equal(t, c.expect, B(input))
 		})
 	}
 }
 
 func TestA(t *testing.T) {
-	t.Skip()
 	input := challenge.ReadChallengeFromFile()
 	assert.Equal(t, 934, A(input))
 }
